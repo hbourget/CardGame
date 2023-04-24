6d49dd75bf7eb890f02c900cf0a7b59cc9370e01
ozaki <29860391+OzakIOne@users.noreply.github.com>
2023-04-24 08:49:09 +0200

    feat: rewrite into modern js

--- front/js/account.js
+++ front/js/account.js
@@ -1,36 +1,79 @@
-$(document).ready(function () {
-  // Récupération de l'inventaire de l'utilisateur
-  $.ajax({
-    url: "http://localhost:8888/inventories/users/1",
-    type: "GET",
-    dataType: "json",
-    success: function (data) {
-      // Pour chaque carte de l'inventaire, récupération des informations de la carte
-      for (var i = 0; i < data.inventory.cards.length; i++) {
-        $.ajax({
-          url: "http://localhost:8888/cards/27",
-          type: "GET",
-          dataType: "json",
-          success: function (cardData) {
-            // Création d'une ligne de tableau avec les informations de la carte
-            var html = "<tr>";
-            html += "<td>" + cardData.id + "</td>";
-            html += "<td>" + cardData.name + "</td>";
-            html += "<td>" + cardData.description + "</td>";
-            html += "<td>" + cardData.power + "</td>";
-            html += "<td>" + cardData.health + "</td>";
-            html += "<td>" + cardData.type + "</td>";
-            html += "<td>" + cardData.energy + "</td>";
-            html += "<td>" + cardData.price + "</td>";
-            html += "<td>" + cardData.image + "</td>";
-            html += "<td> <button>Acheter</button> </td>";
-            html += "<td> <button>Vendre</button> </td>";
-            html += "</tr>";
-            // Ajout de la ligne de tableau au tableau d'affichage
-            $("#account tbody").append(html);
-          },
-        });
-      }
-    },
-  });
+const getCardData = async (cardId) => {
+  try {
+    const response = await fetch(`http://localhost:8888/cards/${cardId}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`Error fetching card data: ${error}`);
+    throw error;
+  }
+};
+
+const getInventoryData = async (userId) => {
+  try {
+    const response = await fetch(
+      `http://localhost:8888/inventories/users/${userId}`,
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`Error fetching inventory data: ${error}`);
+    throw error;
+  }
+};
+
+const createTableCell = (data) => {
+  const cell = document.createElement('td');
+  cell.textContent = data;
+  return cell;
+};
+
+const createTableRow = (cardData) => {
+  const row = document.createElement('tr');
+
+  row.appendChild(createTableCell(cardData.id));
+  row.appendChild(createTableCell(cardData.name));
+  row.appendChild(createTableCell(cardData.description));
+  row.appendChild(createTableCell(cardData.power));
+  row.appendChild(createTableCell(cardData.health));
+  row.appendChild(createTableCell(cardData.type));
+  row.appendChild(createTableCell(cardData.energy));
+  row.appendChild(createTableCell(cardData.price));
+
+  const imageCell = document.createElement('td');
+  const image = document.createElement('img');
+  image.setAttribute('src', cardData.image);
+  image.setAttribute('alt', cardData.name);
+  imageCell.appendChild(image);
+  row.appendChild(imageCell);
+
+  const buyButtonCell = document.createElement('td');
+  const buyButton = document.createElement('button');
+  buyButton.textContent = 'Acheter';
+  buyButtonCell.appendChild(buyButton);
+  row.appendChild(buyButtonCell);
+
+  const sellButtonCell = document.createElement('td');
+  const sellButton = document.createElement('button');
+  sellButton.textContent = 'Vendre';
+  sellButtonCell.appendChild(sellButton);
+  row.appendChild(sellButtonCell);
+
+  return row;
+};
+
+const displayInventory = async () => {
+  const inventoryData = await getInventoryData(1);
+  const inventoryCards = inventoryData.inventory.cards;
+  console.log(inventoryCards);
+  for (const card of inventoryCards) {
+    console.log(card);
+    const cardData = await getCardData(card);
+    const tableRow = createTableRow(cardData);
+    document.querySelector('#account tbody').appendChild(tableRow);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  displayInventory();
 });
--- front/js/cardsList.js
+++ front/js/cardsList.js
@@ -1,75 +1,83 @@
 $(document).ready(function () {
   var table = $('#cardsList').DataTable({
     ajax: {
-      url: "http://localhost:8888/inventories/availablecards",
-      dataSrc: ""
+      url: 'http://localhost:8888/inventories/availablecards',
+      dataSrc: '',
     },
     columns: [
-      { data: "id" },
-      { data: "name" },
-      { data: "description" },
-      { data: "power" },
-      { data: "health" },
-      { data: "type" },
-      { data: "energy" },
-      { data: "price" },
-      { data: "image" },
+      { data: 'id' },
+      { data: 'name' },
+      { data: 'description' },
+      { data: 'power' },
+      { data: 'health' },
+      { data: 'type' },
+      { data: 'energy' },
+      { data: 'price' },
+      { data: 'image' },
       {
         data: null,
         render: function (data, type, row) {
-          return "<form class='buy-form' action='http://localhost:8888/inventories/buy/users/1/cards/" + row.id + "' method='post'>" +
+          return (
+            "<form class='buy-form' action='http://localhost:8888/inventories/buy/users/1/cards/" +
+            row.id +
+            "' method='post'>" +
             "<input type='submit' value='Acheter'>" +
-            "</form>";
-        }
+            '</form>'
+          );
+        },
       },
       {
         data: null,
         render: function (data, type, row) {
-          return "<form class='sell-form' action='http://localhost:8888/inventories/sell/users/1/cards/" + row.id + "' method='post'>" +
+          return (
+            "<form class='sell-form' action='http://localhost:8888/inventories/sell/users/1/cards/" +
+            row.id +
+            "' method='post'>" +
             "<input type='submit' value='Vendre'>" +
-            "</form>";
-        }
-      }
-    ]
+            '</form>'
+          );
+        },
+      },
+    ],
   });
 
   // Ajouter un gestionnaire d'événements pour les formulaires d'achat
-  $("#cardsList").on("submit", ".buy-form", function (event) {
+  $('#cardsList').on('submit', '.buy-form', function (event) {
     event.preventDefault();
     var form = $(this);
     $.ajax({
-      url: form.attr("action"),
-      type: form.attr("method"),
+      url: form.attr('action'),
+      type: form.attr('method'),
       success: function (data) {
-        alert("La carte a été achetée avec succès !");
+        alert('La carte a été achetée avec succès !');
       },
       error: function (xhr, status, error) {
         if (xhr.status == 409) {
           alert("Vous n'avez pas assez d'argent pour acheter cette carte.");
         } else {
-          alert("Une erreur est survenue. Veuillez réessayer plus tard.");
+          alert('Une erreur est survenue. Veuillez réessayer plus tard.');
         }
-      }
+      },
     });
   });
 
   // Ajouter un gestionnaire d'événements pour les formulaires de vente
-  $("#cardsList").on("submit", ".sell-form", function (event) {
+  $('#cardsList').on('submit', '.sell-form', function (event) {
     event.preventDefault();
     var form = $(this);
     $.ajax({
-      url: form.attr("action"),
-      type: form.attr("method"),
+      url: form.attr('action'),
+      type: form.attr('method'),
       success: function (data) {
-        alert("La carte a été vendue avec succès !");
+        alert('La carte a été vendue avec succès !');
       },
       error: function (xhr, status, error) {
         if (xhr.status == 409) {
           alert("Vous ne possédez pas la carte ou la carte n'existe pas.");
         } else {
-          alert("Une erreur est survenue. Veuillez réessayer plus tard.");
+          alert('Une erreur est survenue. Veuillez réessayer plus tard.');
         }
-      }
+      },
     });
   });
 });
