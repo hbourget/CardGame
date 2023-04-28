3a1c224cc54f2f72304d93109e56633b70edc68f
Hugo <hugobourget@protonmail.com>
2023-04-28 11:38:30 +0200

    refactor but still doesnt work

--- micro_service_proxy/src/main/java/com/groupe1/atelier3/config/SecurityConfiguration.java
+++ micro_service_proxy/src/main/java/com/groupe1/atelier3/config/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
@@ -23,11 +24,16 @@ public class SecurityConfiguration {
   private final LogoutHandler logoutHandler;
 
   @Bean
+  public HandlerMappingIntrospector mvcHandlerMappingIntrospector() {
+    return new HandlerMappingIntrospector();
+  }
+
+  @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .csrf().disable()
         .authorizeHttpRequests()
-        .requestMatchers("/**")
+        .requestMatchers("/api/v1/auth/**")
           .permitAll()
         .anyRequest()
           .authenticated()
