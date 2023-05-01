78dadb1ae2e6950cb8ca1f7ee7ea237abeb04f0f
Hugo <hugobourget@protonmail.com>
2023-05-01 19:29:38 +0200

    chore: init dockerization

--- micro_service_proxyauth/src/main/java/com/groupe1/atelier3/config/SecurityConfiguration.java
+++ micro_service_proxyauth/src/main/java/com/groupe1/atelier3/config/SecurityConfiguration.java
@@ -24,12 +24,15 @@ public class SecurityConfiguration {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(Arrays.asList(
-            "http://localhost:8080",
+            "http://proxyauth:8080",
             "http://user:8081",
             "http://card:8082",
             "http://inventory:8083",
             "http://room:8084",
-            "http://localhost:4200"
+            "http://localhost:4200",
+            "http://localhost:80",
+            "http://frontend:80",
+            "http://frontend:4200"
     ));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(Arrays.asList(
